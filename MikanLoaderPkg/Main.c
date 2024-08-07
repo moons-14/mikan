7364cc745017e801407da764003f901ad91269be
moons-14 <moons@moons14.com>
2024-08-07 18:01:05 +0900

    Hello, Mikan World!

---
+++ MikanLoaderPkg/Main.c
@@ -0,0 +1,9 @@
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+  Print(L"Hello, Mikan World!\n");
+
+  while (1);
+  return EFI_SUCCESS;
+}
