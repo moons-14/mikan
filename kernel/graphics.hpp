24bd9940402fc934dd0f17b7bb5cb13c0066659e
moons-14 <moons@moons14.com>
2024-08-13 03:59:18 +0900

    カーソル描画

--- kernel/graphics.hpp
+++ kernel/graphics.hpp
@@ -6,6 +6,20 @@ struct PixelColor
     uint8_t r, g, b;
 };
 
+template <typename T>
+struct Vector2D
+{
+    T x, y;
+
+    template <typename U>
+    Vector2D<T> &operator+=(const Vector2D<U> &rhs)
+    {
+        x += rhs.x;
+        y += rhs.y;
+        return *this;
+    }
+};
+
 class PixelWriter
 {
 public:
@@ -38,3 +52,6 @@ public:
 
     virtual void Write(int x, int y, const PixelColor &c) override;
 };
+
+void FillRectangle(PixelWriter &writer, const Vector2D<int> &pos, const Vector2D<int> &size, const PixelColor &c);
+void DrawRectangle(PixelWriter &writer, const Vector2D<int> &pos, const Vector2D<int> &size, const PixelColor &c);
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -9,6 +9,40 @@
 char console_buf[sizeof(Console)];
 Console *console;
 
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter *pixel_writer;
+
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+    "@              ",
+    "@@             ",
+    "@.@            ",
+    "@..@           ",
+    "@...@          ",
+    "@....@         ",
+    "@.....@        ",
+    "@......@       ",
+    "@.......@      ",
+    "@........@     ",
+    "@.........@    ",
+    "@..........@   ",
+    "@...........@  ",
+    "@............@ ",
+    "@......@@@@@@@@",
+    "@......@       ",
+    "@....@@.@      ",
+    "@...@ @.@      ",
+    "@..@   @.@     ",
+    "@.@    @.@     ",
+    "@@      @.@    ",
+    "@       @.@    ",
+    "         @.@   ",
+    "         @@@   ",
+};
+const PixelColor kDesktopBGColor{45, 118, 237};
+const PixelColor kDesktopFGColor{255, 255, 255};
+
 int printk(const char *format, ...)
 {
     va_list ap;
@@ -32,9 +66,6 @@ void operator delete(void *obj) noexcept
 {
 }
 
-char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
-PixelWriter *pixel_writer;
-
 extern "C" void
 KernelMain(const FrameBufferConfig &frame_buffer_config)
 {
@@ -49,26 +80,33 @@ KernelMain(const FrameBufferConfig &frame_buffer_config)
         break;
     }
 
-    for (int x = 0; x < frame_buffer_config.horizonal_resolution; ++x)
-    {
-        for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y)
-        {
-            pixel_writer->Write(x, y, {255, 255, 255});
-        }
-
-        for (int x = 0; x < 200; ++x)
-        {
-            for (int y = 0; y < 100; ++y)
-            {
-                pixel_writer->Write(x, y, {0, 255, 0});
-            }
-        }
-    }
+    const int kFrameWidth = frame_buffer_config.horizonal_resolution;
+    const int kFrameHeight = frame_buffer_config.vertical_resolution;
+    FillRectangle(*pixel_writer, {0, 0}, {kFrameWidth, kFrameHeight - 50}, kDesktopBGColor);
+    FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth, 50}, {1, 8, 17});
+    FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth / 5, 50}, {80, 80, 80});
+    DrawRectangle(*pixel_writer, {10, kFrameHeight - 40}, {30, 30}, {160, 160, 160});
 
     console = new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
     printk("Hello Mikan OS!\n");
     printk("Horizonal: %d, Vertical: %d\n", frame_buffer_config.horizonal_resolution, frame_buffer_config.vertical_resolution);
+
+    // カーソル表示
+    for (int dy = 0; dy < kMouseCursorHeight; ++dy)
+    {
+        for (int dx = 0; dx < kMouseCursorWidth; ++dx)
+        {
+            if (mouse_cursor_shape[dy][dx] == '@')
+            {
+                pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+            }
+            else if (mouse_cursor_shape[dy][dx] == '.')
+            {
+                pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+            }
+        }
+    }
 
     while (1)
         __asm__("hlt");
